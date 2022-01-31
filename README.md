3534bffa1332ad484382f2e8e3b380f69facd16f
CremaLuca <lc.crema@hotmail.com>
2022-01-31 11:29:49 +0100

    feat(README): Added README file
    refactor: split careers library and automation CLI in two scripts

---
+++ README.md
@@ -0,0 +1,24 @@
+# (Unofficial) UNIPD Careers API library and automation
+
+Collection of libraries to interact with UNIPD Careers API. Contains automation tools (IFTTT) to receive notifications of events.
+
+## Usage
+
+### Careers API library
+
+If you are interested in interacting with the Careers API via Python, you can use the functions available in the `careers.py` library.
+
+#### `get_verification_status`
+
+Parameters:
+
+- `company_vat`: company VAT identification number.
+- `auth`: authorization header line for HTTP request (eg. "Basic AAAA==").
+
+You can browse the `careers.py` script file to see all available methods or use the `__all__` variable.
+
+#### Automation
+
+If you are interested in automating the interaction with the Careers API, the `automation.py` CLI tool is your way to go.
+
+To find all the available functions use `python automation.py --help`.
--- careers.py
+++ careers.py
@@ -1,18 +1,12 @@
-import argparse
+"""
+(Unofficial) UNIPD Careers API library.
+"""
+
+__version__ = "0.1"
+__author__ = "Luca Crema"
+__all__ = ["get_verification_status"]
 
 import requests
-
-
-def __make_parser():
-    parser = argparse.ArgumentParser(
-        description='Checks verification status from the UNIPD Careers API and sends it to a IFTTT webhook',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
-    parser.add_argument('--vats', nargs="+", type=str, help='List of companies VATs.', required=True)
-    parser.add_argument('--auth', help='Authorization token, eg. "Basic AAA===".', required=True)
-    parser.add_argument('--event', type=str, default='verification_update', help='Event name.', required=False)
-    parser.add_argument('--key', type=str, help='IFTTT key.', required=True)
-    return parser
 
 
 def get_verification_status(company_vat: str, auth: str):
@@ -51,27 +45,3 @@ def get_verification_status(company_vat: str, auth: str):
     if "convenzionata" not in response_dict["aziende"][0]:
         raise ValueError(f"Invalid response - missing 'convenzionata' in company details: {response_dict}")
     return response_dict["aziende"][0]["convenzionata"]
-
-
-def main():
-    parser = __make_parser()
-    args = parser.parse_args()
-    ifttt_payload = {}  # Content to be sent to IFTTT
-
-    for vat in args.vats:
-        company_verification_status = get_verification_status(vat, args.auth)
-        print(f"{vat}: {company_verification_status}")
-        ifttt_payload[vat] = company_verification_status
-
-    # Send payload to IFTTT
-    requests.post(
-        url=f"https://maker.ifttt.com/trigger/{args.event}/json/with/key/{args.key}",
-        headers={
-            "Content-Type": "application/json"
-        },
-        json=ifttt_payload
-    )
-
-
-if __name__ == "__main__":
-    main()
